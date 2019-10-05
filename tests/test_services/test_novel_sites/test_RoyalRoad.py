448a4d7f5a55e454b1789bf92ca279dab9f18c0c
shubb <13276972+shubb@users.noreply.github.com>
2019-10-05 18:57:42 +0100

    skeleton for RoyalRoad extractor

--- tests/test_services/test_novel_sites/test_RoyalRoad.py
+++ tests/test_services/test_novel_sites/test_RoyalRoad.py
@@ -1,1 +1,14 @@
 import mock
+
+
+def test_SearchByName(name):
+    assert False
+
+def test_GetTableOfContentsByID(rrd_story_id):
+    assert False
+
+def test_GetChapterTextByURL(chapter_url):
+    assert False
+
+def test_GetStoryByID(rrd_story_id):
+    assert False
