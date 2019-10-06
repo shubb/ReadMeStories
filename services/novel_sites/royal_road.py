f51ee645e37bb53df04909b0cfd1b4637d221263
shubb <13276972+shubb@users.noreply.github.com>
2019-10-06 01:30:03 +0100

    GetChapter for Royal Road

--- services/novel_sites/royal_road.py
+++ services/novel_sites/royal_road.py
@@ -26,18 +26,18 @@ def SearchByName(name_to_search):
     search_results_div = search_soup.find(class_='search-container')
 
     # Pick out the individual search results in a list
-    hmtl_search_results_list = search_results_div.find_all(class_='search-item')
+    soup_search_results_list = search_results_div.find_all(class_='search-item')
 
     # Extract as a list of dicts, the data in each search result
     extracted_search_results = []
-    for html_search_result in hmtl_search_results_list:
+    for soup_search_result in soup_search_results_list:
         
         extracted_search_result = {}
 
         # Within the search result div, find the title, description, and id
-        extracted_search_result['title'] = html_search_result.find('h2').text
-        extracted_search_result['description'] = html_search_result.find(class_='fiction-description').text
-        extracted_search_result['id'] = str(html_search_result.find(class_='fiction-detail')['data-fid'])
+        extracted_search_result['title'] = soup_search_result.find('h2').text
+        extracted_search_result['description'] = soup_search_result.find(class_='fiction-description').text
+        extracted_search_result['id'] = str(soup_search_result.find(class_='fiction-detail')['data-fid'])
 
         extracted_search_results.append(extracted_search_result)
 
@@ -59,21 +59,22 @@ def GetTableOfContentsByID(rrd_story_id):
     toc_soup = BeautifulSoup(toc_response.text, 'html.parser')
 
     # Isolate the tablebody in the chapters table div
-    toc_tbody = toc_soup.find(id='chapters').find('tbody')
+    toc_chapters_div = toc_soup.find(id='chapters')
+    toc_tbody = toc_chapters_div.find('tbody')
 
     # Pick out the individual chapters in a list
-    html_chapters_list = toc_tbody.find_all('tr', recursive=False)
+    soup_chapters_list = toc_tbody.find_all('tr', recursive=False)
 
     # Extract as a list of dicts, the data for each chapter
     extracted_toc = []
-    for chapter_number, html_chapter in enumerate(html_chapters_list):
+    for chapter_number, soup_chapter in enumerate(soup_chapters_list):
         
         extracted_chapter = {}
 
         # Within the chapter html, extract the title and url. Add a synthetic chapter number
         extracted_chapter['chapter_number'] = chapter_number
-        extracted_chapter['title'] = html_chapter.find('td', class_=None).find('a').text
-        extracted_chapter['url'] = html_chapter['data-url']
+        extracted_chapter['title'] = soup_chapter.find('td', class_=None).find('a').text
+        extracted_chapter['url'] = soup_chapter['data-url']
 
         extracted_toc.append(extracted_chapter)
 
@@ -81,8 +82,36 @@ def GetTableOfContentsByID(rrd_story_id):
     return extracted_toc
 
 
+def _remove_attrs(soup):
+    for tag in soup.findAll(True): 
+        tag.attrs = None
+    return soup
+
 def GetChapterTextByURL(chapter_url):
-    pass
+    "Fetch chapter text for the chapter at url chapter_url"
+    
+    # Fetch contents page for the story with the given ID
+    chapter_url = urljoin(RRD_BASE_URL, chapter_url)
+    chapter_response = requests.get(chapter_url)
+
+    # Throw if unsuccesssful
+    chapter_response.raise_for_status()
+
+    # Parse the page into a soup DOM
+    chapter_soup = BeautifulSoup(chapter_response.text, 'html.parser')
+
+    # Get the chapter content
+    soup_chapter_content = chapter_soup.find(class_='chapter-content')
+
+    # TODO: If unclosed div in the chapter content (as in test sample html) may not get whole chapter text. Use regexes instead to extract?
+
+    # Render to text
+    simplified_chapter_text = soup_chapter_content.get_text()
+
+    # Return that text
+    return simplified_chapter_text
+
+
 
 def GetStoryByID(rrd_story_id):
     pass
--- tests/test_services/test_novel_sites/test_RoyalRoad.py
+++ tests/test_services/test_novel_sites/test_RoyalRoad.py
@@ -9,11 +9,11 @@ def test_SearchByName():
     # Mock response so we always get a sample test html instead of really hitting RRD
     with patch('services.novel_sites.royal_road.requests.get') as mock_get:
         mock_get.return_value.ok = True
-        mock_get.return_value.text = GetMockResponse('search_response.html')
+        mock_get.return_value.text = GetMockResponse('rrd_search_response.html')
 
         search_result = royal_road.SearchByName('my title search')
 
-        # Ensure one of the results in the returned dictionary
+        # Ensure one of the results in the returned list
         # has an ID which we know is in the sample test html
         assert filter(lambda story: story['id'] == '17690', search_result)
 
@@ -23,16 +23,29 @@ def test_GetTableOfContentsByID():
     # Mock response so we always get a sample test html instead of really hitting RRD
     with patch('services.novel_sites.royal_road.requests.get') as mock_get:
         mock_get.return_value.ok = True
-        mock_get.return_value.text = GetMockResponse('story_top_page.html')
+        mock_get.return_value.text = GetMockResponse('rrd_story_top_page.html')
 
         toc_result = royal_road.GetTableOfContentsByID('my story id')
 
-        # Ensure one of the results in the returned dictionary
-        # has an ID which we know is in the sample test html
-        assert filter(lambda story: 'Chapter 1 Coming of Age Ceremony' in story['title'], toc_result)
+        # Ensure one of the chapters in the returned list
+        # has a chapter title which we know is in the sample test html
+        assert filter(lambda chapter: 'Chapter 1 Coming of Age Ceremony' in chapter['title'], toc_result)
 
 def test_GetChapterTextByURL():
-    assert False
+    with patch('services.novel_sites.royal_road.requests.get') as mock_get:
+        mock_get.return_value.ok = True
+        mock_get.return_value.text = GetMockResponse('rrd_story_chapter.html')
+
+        chapter_result = royal_road.GetChapterTextByURL('my chapter url')
+
+        # Ensure one of the results in the returned dict
+        # has a story text line which we know is in the sample test html
+        assert filter(lambda chapter: 'I get up and realize I can not move my entire body' in chapter['text'], chapter_result)
 
 def test_GetStoryByID():
-    assert False
+    # This one we will do live on RRD instead of sample htmls
+
+    story_rip = royal_road.GetStoryByID('17690')
+
+    chapter_one = filter(lambda chapter: 'Chapter 1 Coming of Age Ceremony' in chapter['title'])
+
