a26bc117df5cd287f7ae1ff77eb6c75b2286a959
shubb <13276972+shubb@users.noreply.github.com>
2019-10-08 01:13:05 +0100

    Added SpeakChapter (long text) to TextToSpeech

--- services/text_to_speech.py
+++ services/text_to_speech.py
@@ -1,13 +1,51 @@
+import os
+import tempfile
 from google.cloud import texttospeech
+from pydub.generators import Sine
+from pydub import AudioSegment
+from project_root import ROOT_DIR
+from google.oauth2 import service_account
 
 GOOGLE_MAX_SECTION_LENGTH=5000
+GOOGLE_CREDENTIALS_PATH=os.path.join(ROOT_DIR, 'secrets/readnovel-a7ed7aaa0145.json')
+# TODO: Should this path be in config file?
+
+def SpeakChapter(chapter_text, max_section_length=GOOGLE_MAX_SECTION_LENGTH):
+    "Converts a full length chapter text into an mp3"
+
+    # Split the chapter into sections small enough to TTS
+    chapter_sections = SplitTextToSections(chapter_text, max_section_length)
+
+    # Generate an MP3 for each section
+    mp3_sections = []
+    for section in chapter_sections:
+        mp3_sections.append(SpeakSection(section))
+
+    # Combine the sections into a single mp3
+    mp3_chapter = Sine(300).to_audio_segment(duration=500)
+    for mp3_section in mp3_sections:
+        mp3_chapter = mp3_chapter.append(AudioSegment.from_mp3(mp3_section.name))
+
+    # Clean up the section temp files
+    for mp3_section in mp3_sections:
+        os.unlink(mp3_section.name)
+
+    # Return the full Mp3 (as a temporary file)
+    temporary_mp3 = tempfile.NamedTemporaryFile(suffix='.mp3', delete=False)
+    mp3_chapter.export(temporary_mp3.name, format="mp3")
+
+    return temporary_mp3
 
 def SpeakSection(section_text):
+    "Converts a short section text into an mp3"
     if len(section_text) > GOOGLE_MAX_SECTION_LENGTH:
         raise Exception("Section too big, text must be broken into units of ${GOOGLE_MAX_SECTION_LENGTH}")
 
+    # Load google credentials
+    credentials = service_account.Credentials.from_service_account_file(GOOGLE_CREDENTIALS_PATH)
+
     # Instantiates a client
-    client = texttospeech.TextToSpeechClient()
+    client = texttospeech.TextToSpeechClient(credentials=credentials)
 
     # Set the text input to be synthesized
     synthesis_input = texttospeech.types.SynthesisInput(text=section_text)
@@ -29,7 +67,14 @@ def SpeakSection(section_text):
     # voice parameters and audio file type
     response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
-    return response.audio_content
+    temporary_mp3 = tempfile.NamedTemporaryFile(suffix='.mp3', delete=False)
+    temporary_mp3.write(response.audio_content)
+    temporary_mp3.close()
+
+    # Needs to be deleted later
+    # TODO: What is better way to do this? Does it work without delete=False
+    # Return the section mp3 (as a temporary file)
+    return temporary_mp3
 
 def _is_blank(text):
     return text is None or text.strip() == ''
@@ -65,4 +110,10 @@ def SplitTextToSections(full_text, max_section_length):
     return final_list
 
 def CleanUpTextForTTS(full_text):
+    # TODO: remove things that typically cause bad tts
+    # section breaks
+    # URLs
+    # Long non-words
+    # detect and handle acronyms nicely?
+
     pass
--- tests/test_services/test_TextToSpeech.py
+++ tests/test_services/test_TextToSpeech.py
@@ -6,11 +6,25 @@ def test_SpeakSelection():
 
     # Create an MP3 speech saying section_text
     section_text = "Some text to say"
-    mp3_output = TextToSpeech.SpeakSection(section_text)
+
+    tempfile_mp3_output = TextToSpeech.SpeakSection(section_text)
 
     # Sniff test the MP3 by checking it is about the right size
-    assert len(mp3_output) > 3000
-    assert len(mp3_output) < 10000
+    assert os.stat(tempfile_mp3_output.name).st_size > 3000
+    assert os.stat(tempfile_mp3_output.name).st_size < 10000
+    os.unlink(tempfile_mp3_output.name)
+
+def test_SpeakChapter():
+    
+    # Should be split into 5 paragraphs before synthesis
+    sample_text = ''.join([ 'cats and dogs.' * 10 + os.linesep * 2 for i in range(5) ])
+
+    tempfile_mp3_output = TextToSpeech.SpeakChapter(sample_text)
+
+    # Sniff test the MP3 by checking it is about the right size
+    assert os.stat(tempfile_mp3_output.name).st_size > 204919
+    assert os.stat(tempfile_mp3_output.name).st_size < 404919    
+    os.unlink(tempfile_mp3_output.name)
 
 def test_SplitTextToSections():
 
