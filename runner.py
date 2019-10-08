3813fcce174a0c2c91fecd158909ac026b7f1098
shubb <13276972+shubb@users.noreply.github.com>
2019-10-08 01:55:07 +0100

    Working demo

--- runner.py
+++ runner.py
@@ -1,5 +1,17 @@
+import os
+from shutil import copyfile
 import services.text_to_speech as TextToSpeech
+import services.novel_sites.royal_road as RoyalRoad
 
-output = TextToSpeech.SpeakSection("Some text to say")
 
-print(len(output))
+def NovelToAudiobook():
+    
+    #Fetch an actual story
+    story_rip = RoyalRoad.GetStoryByID('17690')
+
+    for chapter in story_rip:
+        temp_mp3 = TextToSpeech.SpeakChapter(chapter)
+        copyfile(temp_mp3.name, os.path.join('/home/tim/audiobook/Chapter_{}.mp3'.format(chapter['chapter_number'])))
+        os.unlink(temp_mp3.name)
+
+NovelToAudiobook()
