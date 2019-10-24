08039a2ace539b57cf4e0fe820bded505f0d8b93
shubb <13276972+shubb@users.noreply.github.com>
2019-10-24 03:11:04 +0100

    Added implimentation and tests for Epub source

--- services/novel_sites/uploaded_epub.py
+++ services/novel_sites/uploaded_epub.py
@@ -1,10 +1,60 @@
+import ebooklib
+from ebooklib import epub
+from bs4 import BeautifulSoup
 
 def GetTableOfContentsFromEpub(epub):
     "Given an Epub file attempt to build a TOC"
 
-def GetChapterTexFromChapter(epub_html_chapter):
+    epup_docs_unfiltered = epub.get_items_of_type(ebooklib.ITEM_DOCUMENT)
+    epup_html_chapters = [ document for document in epup_docs_unfiltered if document.is_chapter() ]
+
+    extracted_toc = []
+    for chapter_number, epup_html_chapter in enumerate(epup_html_chapters):
+        
+        extracted_chapter = {}
+
+        # Within the chapter html, extract the title and url. Add a synthetic chapter number
+        extracted_chapter['chapter_number'] = chapter_number
+        extracted_chapter['title'] = epup_html_chapter.get_id()
+        extracted_chapter['html_chapter'] = epup_html_chapter
+
+        extracted_toc.append(extracted_chapter)
+
+    # Return a list of chapters, with title, content as a EpubHTML, and a synthetic chapter number
+    return extracted_toc
+
+
+def GetChapterTexFromEpubHTMLChapter(chapter_dict):
     "Given a chapter in EpubHTML form, extract chapter text"
+
+    epub_html_chapter = chapter_dict['html_chapter']
+    chapter_soup = BeautifulSoup(epub_html_chapter.get_content(), 'html.parser')
+    body_soup = chapter_soup.find('body')
+
+    return body_soup.text
     
 
 def GetStoryByLocalPath(epub_path):
     "Load the epub at epub_path and extract chapters"
+
+    epub_book = epub.read_epub(epub_path)
+
+    # Get the TOC for this story
+    story = GetTableOfContentsFromEpub(epub_book)
+
+    # For each chapter in the TOC, fetch the text and return
+    # a list containing the TOC data for the chapter plus the text
+    for chapter in story:
+        chapter['text'] = GetChapterTexFromEpubHTMLChapter(chapter)
+
+    return story
+
+""" 
+    # Extract compulsory title metadata attribute
+    title = epub.get_metadata('DC', 'title')[0][0]
+    
+    description = "No description"
+    try:
+        description = epub.get_metadata('DC', 'title')[0][0]
+    except:
+        pass """
--- tests/test_services/test_novel_sites/test_UploadEpub.py
+++ tests/test_services/test_novel_sites/test_UploadEpub.py
@@ -1,34 +1,35 @@
-import pprint
-from unittest.mock import patch
+import ebooklib
+from ebooklib import epub
 from tests.test_assets import GetTestEpubPath
 from services.novel_sites import uploaded_epub
 
 
 def test_GetTableOfContentsFromEpub():
-    toc_result = uploaded_epub.GetTableOfContentsFromEpub(GetTestEpubPath())
+    book = epub.read_epub(GetTestEpubPath())
+    toc_result = uploaded_epub.GetTableOfContentsFromEpub(book)
 
-    # Ensure one of the chapters in the returned list
-    # has a chapter title which we know is in the sample test html
-    assert len(toc_result) == 5
+    # there should be 305 chapters in this story
+    assert len(toc_result) == 305
 
-def test_GetChapterTexFromChapter():
+def test_GetChapterTexFromEpubHTMLChapter():
     
-    toc_result = uploaded_epub.GetTableOfContentsFromEpub(GetTestEpubPath())
-    chapter_result = uploaded_epub.GetChapterTexFromChapter(toc_result[0])
+    book = epub.read_epub(GetTestEpubPath())
+    toc_result = uploaded_epub.GetTableOfContentsFromEpub(book)
+    chapter_result = uploaded_epub.GetChapterTexFromEpubHTMLChapter(toc_result[1])
 
     # Ensure one of the results in the returned dict
     # has a story text line which we know is in the sample test html
-    assert filter(lambda chapter: 'I get up and realize I can not move my entire body' in chapter['text'], chapter_result)
+    assert filter(lambda chapter: 'Gladly and back to the clock.' in chapter['text'], chapter_result)
 
 def test_GetStoryByLocalPath():
        # Rip story 17690
-    story_rip = royal_road.GetStoryByLocalPath(GetTestEpubPath())
+    story_rip = uploaded_epub.GetStoryByLocalPath(GetTestEpubPath())
 
     # Grab the first chapter which should be called 'A Meaningless Box'
-    chapter_one = next(filter(lambda chapter: 'Meaningless Box' in chapter['title'], story_rip))
+    chapter_one = next(filter(lambda chapter: 'chapter000' in chapter['title'], story_rip))
     
     # First chapter should contain this text
-    assert 'oversized black cloak' in chapter_one['text']
+    assert 'Gladly and back to the clock.' in chapter_one['text']
     
-    # there should be at least 2 chapters in this story
-    assert len(story_rip) > 2
+    # there should be 305 chapters in this story
+    assert len(story_rip) == 305
